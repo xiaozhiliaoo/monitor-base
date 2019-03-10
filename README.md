2457ad253a182a318c8a931ca2aa8896e2ba8855
xiaozhiliaoo <xiaozhiliaoo@gmail.com>
2019-03-10 19:24:19 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # monitor-base
 springboot2,exporter,prometheus,grafana搭建监控体系
+
+####step1 搭建springboot2程序 
+   [application.yaml](src/main/resources/application.yaml)
+####step2 node_exporter安装
+####step3 mysql_exporter安装
+####step4 prometheus安装和配置  
+   [prometheus.yml](src/main/resources/prometheus.yml)
+####step5 grafana安装
+####step6 自定义panel:jvm,mysql,cpu相关指数
+
+
---
+++ src/main/java/org/lili/Application.java
@@ -0,0 +1,27 @@
+package org.lili;
+
+/**
+ * @author lili
+ * @description ${DESCRIPTION}
+ * @create 2019-03-09 12:59
+ * @since
+ **/
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@Log4j2
+public class Application {
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(Application.class, args);
+        } catch (Throwable e) {
+            log.error("", e);
+        }
+    }
+}
